9e23c557243a3fc07fd3d09fc1968da5acec1679
Christian Kulat <cdkulat1@gmail.com>
2023-02-09 08:49:41 -0500

    bot wireframe nearly complete - can read guild members and guild name

--- bot.py
+++ bot.py
@@ -1,17 +1,28 @@
 import os
+
 import discord
 from dotenv import load_dotenv
 
-intents = discord.Intents.default()
-client = discord.Client(intents = intents)
-
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 
+intent = discord.Intents.default()
+client = discord.Client(intents = intent)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})\n'
+    )
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(TOKEN)
